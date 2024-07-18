2546174a8f717a565661f5f02632614d5e8ccec3
ahmadihsanullah <ahma21327ti@student.nurulfikri.ac.id>
2024-07-18 09:47:48 +0700

    belajar integrasi spring security dengan keyloack

---
+++ src/main/java/com/integrasi/springsecurity/keyloack/IntegrateSpringBoot3DenganKeyloackApplication.java
@@ -0,0 +1,13 @@
+package com.integrasi.springsecurity.keyloack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrateSpringBoot3DenganKeyloackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrateSpringBoot3DenganKeyloackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/integrasi/springsecurity/keyloack/IntegrateSpringBoot3DenganKeyloackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.integrasi.springsecurity.keyloack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrateSpringBoot3DenganKeyloackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
